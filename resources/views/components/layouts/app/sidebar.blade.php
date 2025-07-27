c6494d590f553fc152d266b5117547eabf64a14e
Apriniasr <apriniasalsabila99@gmail.com>
2025-07-27 19:16:58 +0700

    editing admin UI

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -9,7 +9,7 @@
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
             <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
-                <x-app-logo />
+                UMKM Alun-Alun Contong
             </a>
 
             <flux:navlist variant="outline">
@@ -50,16 +50,6 @@
 
 
             <flux:spacer />
-
-            <!-- <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
-
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
-                </flux:navlist.item>
-            </flux:navlist> -->
 
             <!-- Desktop User Menu -->
             <flux:dropdown class="hidden lg:block" position="bottom" align="start">
@@ -157,6 +147,7 @@
             </flux:dropdown>
         </flux:header>
 
+        
         {{ $slot }}
 
         @fluxScripts
